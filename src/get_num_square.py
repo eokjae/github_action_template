68c2440260071c47115e323d0a4a82bb9bc5f2d0
eokjae <114716862+eokjae@users.noreply.github.com>
2023-04-04 15:46:55 +0900

    Update get_num_square.py

--- src/get_num_square.py
+++ src/get_num_square.py
@@ -5,9 +5,9 @@ num = os.environ.get("INPUT_NUM")
 if num:
     try:
        num = int(num)
-       except Exception:
-         exit()
-     else:
-        num = 1
-        
-  print(f"::set-output name=num_squared::{num ** 2}")
+    except Exception:
+       exit()
+else:
+    num = 1
+    
+print(f"::set-output name=num_squared::{num ** 2}")
